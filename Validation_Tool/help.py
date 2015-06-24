4bc4406768a58d446dfe104c2b03cbb8854c3d79
René Kopeinig <renekope@gmail.com>
2015-06-23 22:26:26 -0500

    Update help.py
    - better input explaination

--- Validation_Tool/help.py
+++ Validation_Tool/help.py
@@ -1,5 +1,5 @@
 input_h = "<b>Input data<\b>"
-input = "Select either ESRI Shapefile (.shp) or ESRI Shapefile attribute format (.dbf). Make sure about the correctness of reference and predicted column number, by opening Quantum GIS and check the attribute table.<br>"
+input = "Select either ESRI Shapefile (.shp) or ESRI Shapefile attribute format (.dbf). By opening the input in QGIS, please check the attribute table and make sure about the correctness of referenced and predicted column numbers. Referenced and predicted represents the classified and the predicted class for each classified object.<br>"
 
 output_h = "<b>Output data<\b>"
 output = "The validation output will be stored as CSV File. If you do not set any output, it will be named after your input file plus the suffix '_validation.csv'<br>"
@@ -11,4 +11,4 @@ validation_h = "<b>Validation<\b>"
 validation = "Validation algorithm is based on the scientific work of P. Olofsson named 'Making better use of accuracy data in land change studies: Estimating accuracy and area quantifying uncertainty using stratified estimation'. The algorithm considers reference data to estimate the area per class. The user is able to choose any reference data in two different ways, set them manually or upload a CSV file.<br>"
 
 info_h = "<b>Contact<\b>"
-info = "For further information, feel free to contact the author of this plugin (Rene Kopeinig, rkopeinig@conabio.gob.mx)"
+info = "For further information, feel free to contact the author of this plugin (Rene Kopeinig, renekope@gmail.com or rkopeinig@conabio.gob.mx)"
